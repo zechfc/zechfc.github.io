698d2eed7f51c0ee17cbf5f1904ec571c2309838
zechfc <zechfc@email.com>
2024-02-04 23:51:18 -0500

    finshed the overall template still need to add CSS

--- csce242/projects/part3/script.js
+++ csce242/projects/part3/script.js
@@ -1,11 +1,7 @@
-//let- variable that can change
-//const- variable that does not change (constant)
-//when putting parathesis at the end of the function it calls the function
-
-//put all functions at the top
+//Javascript for later
 const signin = () =>{
-    alert("login");
-
+    const root = document.querySelector(":root");
+    root.style.setProperty("--square-top", pos + "px");
 };
 
 const cart = () => {
@@ -13,6 +9,63 @@ const cart = () => {
 
   
 };
+const Order = () => {
+    alert("added to order");  
+};
 
-document.getElementById("signin").onclick = signin;
-document.getElementById("cart-btn").onclick = cart;
+const fade = () => {
+    document.getElementById("pic1").style.display = "none";
+
+  
+};
+
+const show = () => {
+    document.getElementById("pic1").style.display = "block";
+
+  
+};
+const fade2 = () => {
+    document.getElementById("pic2").style.display = "none";
+
+  
+};
+const show2 = () => {
+    document.getElementById("pic2").style.display = "block";
+  
+};
+
+const toggleMenu = () => {
+        const mainNav= document.getElementById("main-nav");
+        mainNav.classList.toggle("show-menu");
+    };
+
+const pickmeup = () => {
+    alert("test");
+    window.open("login.html");
+  
+};
+const putmedown = () => {
+    alert("test");
+
+    window.open("cart.html");
+  
+};
+
+
+//document.getElementById("signin").onclick = signin;
+//document.getElementById("cart-btn").onclick = cart;
+document.getElementById("pic1").onmouseenter = fade;
+document.getElementById("pic1").onmouseleave = show;
+
+document.getElementById("pic2").onmouseenter = fade2;
+document.getElementById("pic2").onmouseleave = show2;
+
+document.getElementById("btn-day").onclick = pickmeup;
+document.getElementById("btn-night").onclick = putmedown;
+
+document.getElementById("order1").onclick = Order;
+document.getElementById("order2").onclick = Order;
+document.getElementById("order3").onclick = Order;
+document.getElementById("order4").onclick = Order;
+
+document.getElementById("hamburger-btn").onclick = toggleMenu;
