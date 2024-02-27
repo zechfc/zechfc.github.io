c903364c7c1a942c170cc7ab965302636b30f3f2
zechfc <zechfc@email.com>
2024-02-27 09:09:08 -0500

    feb27 example

--- csce242/example/Feb22/gallery-script.js
+++ csce242/example/Feb22/gallery-script.js
@@ -3,6 +3,30 @@ document.querySelectorAll(".items section").forEach((section) =>{
     section.onclick = (e) => {
         document.getElementById("dialog").style.display = "block";
         
+        //const src = section.querySelector("img");
+        //e = what was clicked to start the action
+        //const src = e.currentTarget.querySelector("img").src;
+        //console.log(src);
+        
         document.querySelector("#dialog-content img").src = e.currentTarget.querySelector("img").src;
+        
+        const detailsSection = document.getElementById("dialog-details");
+        const h3 = document.createElement("h3");
+        h3.innerHTML = section.querySelector("h3").innerHTML;
+        detailsSection.append(h3);
+
+        const ps = section.querySelectorAll("p");
+
+        ps.forEach((p) => {
+            const newP = document.createElement("p");
+            newP.innerHTML = p.innerHTML;
+            detailsSection.append(newP);
+        });
+
+    
     };
 });
+
+document.getElementById("dialog-close").onclick = () => {
+    document.getElementById("dialog").style.display = "none";
+};
