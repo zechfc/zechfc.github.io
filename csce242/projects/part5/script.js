38ef8d8d2e3ab31ecffb814bae9b0600a8799604
zechfc <zechfc@email.com>
2024-03-21 08:39:36 -0400

    added a dupe project five

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -18,6 +18,48 @@ const showHideNav = () => {
 
 };
 
+const showEmailResult = async(e) => {
+    e.preventDefault();
+
+    const result = document.getElementById("result");
+    let response = await getElementResult();
+
+    if(response.status == 200){
+        result.innerHTML = "Email successfully sent";
+    } else {
+        result.innerHTML = "Sorry, your email was not sent";
+    }
+};
+
+const getElementResult = async () => {
+    const form = document.getElementById("contact-form");
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Please wait ....";
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                Accept:"application/json"
+            },
+            body:json
+            
+
+        });
+
+        return response;
+    } catch(error){
+        console.log(error);
+        result.innerHTML = "Sorry, your email couldn't be sent";
+    }
+
+};
+
 const slideForwardFood = () => {
     //at the end of the list of images
     if(getCurrentImageFood().nextElementSibling == null)
@@ -1160,7 +1202,7 @@ if(document.getElementById("btn-attributions"))
     document.getElementById("btn-attributions").onclick = attributions;
 }
 
-
+document.getElementById("contact-form").onsubmit = showEmailResult;
 // document.getElementById("forward-arrow-drink").onclick = slideForwardDrink;
 // document.getElementById("backward-arrow-drink").onclick = slideBackwardDrink;
 
