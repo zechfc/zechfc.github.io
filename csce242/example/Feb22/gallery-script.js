8f621c062db28050fdcc0f8ff92195f9bce6a14c
Zechariah Fisher-Coleman <zechfc@gmail.com>
2024-03-26 03:35:40 -0400

    updated main page

--- csce242/example/Feb22/gallery-script.js
+++ csce242/example/Feb22/gallery-script.js
@@ -1,31 +1,23 @@
-//https:www.w3schools.com/w3css model.asp
-document.querySelectorAll(".items section").forEach((section) =>{
+//https://www.w3schools.com/w3css/w3css_modal.asp
+
+document.querySelectorAll(".items section").forEach((section) => {
     section.onclick = (e) => {
-        document.getElementById("dialog").style.display = "block";
-        
-        //const src = section.querySelector("img");
-        //e = what was clicked to start the action
-        //const src = e.currentTarget.querySelector("img").src;
-        //console.log(src);
-        
-        document.querySelector("#dialog-content img").src = e.currentTarget.querySelector("img").src;
-        
-        const detailsSection = document.getElementById("dialog-details");
-        const h3 = document.createElement("h3");
-        h3.innerHTML = section.querySelector("h3").innerHTML;
-        detailsSection.append(h3);
-
-        const ps = section.querySelectorAll("p");
-
-        ps.forEach((p) => {
-            const newP = document.createElement("p");
-            newP.innerHTML = p.innerHTML;
-            detailsSection.append(newP);
-        });
-
-    
+      const dialog = (document.getElementById("dialog").style.display = "block");
+      document.querySelector("#dialog-content img").src =
+        e.currentTarget.querySelector("img").src;
+  
+      const dialogDetails = document.querySelector("#dialog-details");
+      dialogDetails.innerHTML = "";
+  
+      const h3 = document.createElement("h3");
+      h3.innerHTML = e.currentTarget.querySelector("h3").innerHTML;
+      dialogDetails.append(h3);
+  
+      const p = document.createElement("p");
+      p.innerHTML = e.currentTarget.querySelector("p").innerHTML;
+      dialogDetails.append(p);
     };
-});
+  });
 
 document.getElementById("dialog-close").onclick = () => {
     document.getElementById("dialog").style.display = "none";
